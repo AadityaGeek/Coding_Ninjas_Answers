f3c076d9582c4b808a53704ba9bb1f4bb23ab567
Aaditya Kumar <127582609+AadityaGeek@users.noreply.github.com>
2024-06-18 22:39:44 +0530

    Create Find Area of Rectangle.py

---
+++ Python/Find Area of Rectangle.py
@@ -0,0 +1,13 @@
+# Author : Aaditya Kumar
+# Question : Find Area of Rectangle
+# Question Link : https://www.naukri.com/code360/guided-paths/basics-of-python/content/118790/offering/1461384?leftPanelTab=0
+
+# Solution
+#Your code goes here
+l,b = map(float,input().split(' '))
+
+#Calculating area
+area = l * b
+
+#print the area
+print(int(area))
